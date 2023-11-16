e926a2742e6f7d8a55017c3eae02b39f6bf5b73c
Olha Momot <helgammt@gmail.com>
2023-11-17 00:28:17 +0200

    Create folder structure for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import './App.scss';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Jopa
-      </h1>
+      <Header />
     </div>
   );
 }
